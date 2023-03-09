4d7f8be2ab01b7225c29936803119f53b933d275
mfw78 <mfw78@protonmail.com>
2023-03-09 13:11:00 +0000

    feat: overlay struct and impl

--- src/overlay.rs
+++ src/overlay.rs
@@ -1,4 +1,10 @@
-use ethers::{types::H160, utils::keccak256};
+use ethers::{types::H160, utils::keccak256, prelude::k256::ecdsa::SigningKey};
+use eyre::{Result, anyhow};
+
+use ethers::prelude::*;
+use passwords::PasswordGenerator;
+
+use crate::topology::Topology;
 
 pub type Nonce = [u8; 32];
 
@@ -21,6 +27,139 @@ impl Overlay for H160 {
 
         // return the hash
         keccak256(data)
+    }
+}
+
+pub struct MinedAddress {
+    wallet: Wallet<SigningKey>,
+    nonce: Option<Nonce>,
+    password: String,
+}
+
+impl MinedAddress {
+    pub fn new(radius: u32, neighbourhood: u32, network_id: u32, nonce: Option<Nonce>) -> Result<Self> {
+        let store = Topology::new(radius);
+
+        // guard against invalid neighbourhoods
+        if neighbourhood >= store.num_neighbourhoods() {
+            return Err(anyhow!(
+                "Invalid neighbourhood {} for radius {}. Max neighbourhood is {}",
+                neighbourhood,
+                radius,
+                store.num_neighbourhoods() - 1
+            ));
+        }
+
+        println!(
+            "Mining overlay address for neighbourhood {}/{}",
+            neighbourhood,
+            store.num_neighbourhoods() - 1
+        );
+
+        // get the base overlay address for the target neighbourhood and depth
+        let base_overlay_address = store.get_base_overlay_address(neighbourhood);
+
+        // calculate the bit-mask for the depth
+        let bit_mask = store.neighbourhood_bitmask();
+
+        let pg = PasswordGenerator {
+            length: 32,
+            numbers: true,
+            lowercase_letters: true,
+            uppercase_letters: true,
+            symbols: false,
+            spaces: false,
+            exclude_similar_characters: true,
+            strict: true,
+        };
+
+        let password = pg.generate_one().unwrap();
+
+        // create a temporary directory to store the keystore
+        let dir = tempfile::tempdir()?;
+        let path = dir.path();
+
+        let mut count = 0;
+
+        loop {
+            // increment the count
+            count += 1;
+
+            // create a new keystore with the password
+            let (wallet, uuid) = LocalWallet::new_keystore(
+                path,
+                &mut rand::thread_rng(),
+                password.clone(),
+                None,
+            )?;
+
+            // calculate the overlay address for the keypair
+            let overlay_address = wallet.address().overlay_address(network_id, nonce);
+
+            // use the bit mask to compare the overlay address to the base overlay address
+            let mut match_found = true;
+            for i in 0..32 {
+                if overlay_address[i] & bit_mask[i]
+                    != base_overlay_address[i] & bit_mask[i]
+                {
+                    match_found = false;
+                    break;
+                }
+            }
+
+            // if a match was found, print the keypair and exit
+            if match_found {
+                // if a match was found, print the keypair and exit
+                println!("Match found after {} iterations...", count);
+
+                // get the current directory
+                let current_dir = std::env::current_dir()?;
+
+                // get the path to the keystore
+                let keystore_path = path.join(uuid);
+
+                // copy the keystore to the current directory and give it the name `overlay_address.json`
+                std::fs::copy(
+                    keystore_path,
+                    current_dir.join(format!("{}.json", hex::encode(overlay_address))),
+                )?;
+
+                // write the password to a file
+                std::fs::write(
+                    current_dir
+                        .join(format!("{}.password", hex::encode(overlay_address))),
+                    password.clone(),
+                )?;
+
+                dir.close()?;
+
+                return Ok(Self {
+                    wallet,
+                    nonce,
+                    password,
+                });
+            }
+        }
+    }
+
+    pub fn wallet(&self) -> &Wallet<SigningKey> {
+        &self.wallet
+    }
+
+    pub fn overlay(&self, network_id: u32) -> crate::Overlay {
+        self.wallet.address().overlay_address(network_id, self.nonce)
+    }
+
+    pub fn password(&self) -> &str {
+        &self.password
+    }
+
+    pub fn address(&self) -> H160 {
+        self.wallet.address()
+    }
+
+    pub fn private_key(&self) -> [u8; 32] {
+        self.wallet.signer().to_bytes().into()
     }
 }
 
