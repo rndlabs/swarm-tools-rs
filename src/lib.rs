b372beea57ae61240966e24b2cb5bb7f1fe4164d
mfw78 <mfw78@protonmail.com>
2023-03-27 15:18:03 +0000

    feat: wallet module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 use ethers::prelude::*;
 use eyre::Result;
-use std::str::FromStr;
+use std::{str::FromStr};
+use clap::{Args, Parser, Subcommand};
+
+use crate::{topology::Topology, redistribution::get_avg_depth, overlay::{MinedAddress, OverlayCalculator}, game::Game, postage::PostOffice};
 
 pub mod chain;
 pub mod contracts;
@@ -9,9 +12,208 @@ pub mod overlay;
 pub mod postage;
 pub mod redistribution;
 pub mod topology;
+pub mod wallet;
+
 pub type OverlayAddress = [u8; 32];
 
-pub type Overlay = [u8; 32];
+const POSTAGESTAMP_START_BLOCK: &str = "25527076";
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Calculators / tools for neighbourhoods
+    Topology(TopologyArgs),
+    /// Calculators / miner for overlays
+    Overlay(OverlayArgs),
+    /// Analyse neighbourhood distribution for the schelling game
+    #[command(arg_required_else_help = true)]
+    Game {
+        /// The address of the stake registry contract
+        #[arg(long, value_parser = parse_name_or_address)]
+        stake_registry: Option<H160>,
+        /// Storage radius for analysis
+        #[arg(short, default_value = "8")]
+        radius: u32,
+        /// RPC to connect to
+        #[arg(long, default_value = "http://localhost:8545")]
+        rpc: String,
+    },
+    /// Analyse postage stamps
+    #[command(arg_required_else_help = true)]
+    PostageStamp {
+        /// The address of the postage stamp contract
+        #[arg(long, value_parser = parse_name_or_address)]
+        postage_stamp_contract_address: Option<H160>,
+        /// The block to start analysis from
+        #[arg(long, default_value = POSTAGESTAMP_START_BLOCK)]
+        start_block: u64,
+        /// RPC to connect to
+        #[arg(long, default_value = "http://localhost:8545")]
+        rpc: String,
+    },
+    /// Node funding related tools
+    Wallet(WalletArgs),
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+pub struct TopologyArgs {
+    #[command(subcommand)]
+    command: TopologyCommands,
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+pub struct OverlayArgs {
+    #[command(subcommand)]
+    command: OverlayCommands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum TopologyCommands {
+    /// Given a radius, output the base overlay address for all neighbourhoods
+    DumpBaseOverlays(RadiusArgs),
+    /// Given a radius, output the number of neighbourhoods
+    NumNeighbourhoods(RadiusArgs),
+    /// Calculate the daily average reported storage radius
+    ActualAvgStorageRadius {
+        /// The address of the stake registry contract
+        #[arg(long, value_parser = parse_name_or_address)]
+        redistribution_address: Option<H160>,
+        /// RPC to connect to
+        #[arg(long, default_value = "http://localhost:8545")]
+        rpc: String,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+pub enum OverlayCommands {
+    /// Calculate an overlay address from it's components
+    Calculate {
+        #[arg(
+            short,
+            help = "The ethereum address for the overlay",
+            value_parser = parse_name_or_address
+        )]
+        address: H160,
+        #[arg(short, help = "The Swarm network ID")]
+        network_id: u32,
+        #[arg(
+            long,
+            help = "A nonce (for grand-fathered nodes, no longer used)",
+            value_parser = parse_bytes32
+        )]
+        nonce: Option<[u8; 32]>,
+    },
+    /// Determine which neighbourhood an overlay is in
+    Neighbourhood {
+        #[arg(
+            short,
+            help = "The radius to calculate the neighbourhood population with"
+        )]
+        radius: u32,
+        #[arg(
+            help = "The overlay from which to determine the neighbourhood with",
+            value_parser = parse_bytes32
+        )]
+        overlay: OverlayAddress,
+    },
+    /// Automatically mine overlay addresses into the optimal neighbourhoods
+    /// for a given radius
+    AutoMine {
+        #[arg(short, help = "The number of addresses to mine")]
+        num_addresses: u32,
+        #[arg(long, help = "The Swarm network ID")]
+        network_id: u32,
+        /// RPC to connect to
+        #[arg(long, default_value = "http://localhost:8545")]
+        rpc: String,
+    },
+    /// Mine an overlay address into a specific neighbourhood
+    ManualMine {
+        #[arg(short, help = "The radius to calculate the neighbourhoods with")]
+        radius: u32,
+        #[arg(short, help = "The neighbourhood to mine the address into")]
+        neighbourhood: u32,
+        #[arg(long, help = "The Swarm network ID")]
+        network_id: u32,
+        #[arg(
+            long,
+            help = "The nonce, if any to use for mining",
+            value_parser = parse_bytes32
+        )]
+        nonce: Option<[u8; 32]>,
+    },
+}
+
+#[derive(Debug, Args)]
+pub struct RadiusArgs {
+    #[arg(short, help = "The radius used for determining neighbourhood topology")]
+    radius: u32,
+}
+
+#[derive(Debug, Args)]
+#[command(args_conflicts_with_subcommands = true)]
+pub struct WalletArgs {
+    #[command(subcommand)]
+    pub command: WalletCommands,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum WalletCommands {
+    /// Generate a new funding wallet
+    Generate,
+    /// Initialize a Safe wallet on Gnosis Chain
+    InitSafe {
+        /// RPC to connect to
+        #[arg(long, default_value = "http://localhost:8545")]
+        rpc: String,
+    },
+    /// Given the bee node wallets to be funded, calculate the required amount of DAI / ETH
+    /// to fund them all.
+    CalculateFundingRequirements {
+        #[arg(short, help = "Set a maximum amount of BZZ to fund each node with")]
+        max_bzz: Option<U256>,
+        #[arg(short, help = "Set the amount of xDAI to fund each node with")]
+        xdai: Option<U256>,
+        #[arg(long, default_value = "http://localhost:8545", help = "RPC to connect to")]
+        rpc: String,
+    },
+    /// Swap and bridge the required amount of DAI to BZZ and then bridge the BZZ to xDAI
+    SwapAndBridge {
+        #[arg(long, default_value = "http://mainnet:8545", help = "Ethereum Mainnet RPC to connect to")]
+        mainnet_rpc: String,
+        #[arg(long, default_value = "http://localhost:8545", help = "Gnosis Chain RPC to connect to")]
+        gnosis_rpc: String,
+        #[arg(short, help = "Set a maximum amount of BZZ to fund each node with")]
+        max_bzz: Option<U256>,
+        #[arg(short, help = "Set the amount of xDAI to fund each node with")]
+        xdai: Option<U256>,
+    },
+    /// Fund all the bee nodes with funds in a Safe wallet.
+    DistributeFunds {
+        #[arg(short, help = "Set a maximum amount of BZZ to fund each node with")]
+        max_bzz: Option<U256>,
+        #[arg(short, help = "Set the amount of xDAI to fund each node with")]
+        xdai: Option<U256>,
+        #[arg(long, default_value = "http://localhost:8545", help = "RPC to connect to")]
+        rpc: String,
+    },
+    /// Set all BZZ approves on the node wallets (Safe wallet and StakeRegistry).
+    PermitApproveAll {
+        #[arg(long, default_value = "http://localhost:8545", help = "RPC to connect to")]
+        rpc: String,
+    },
+    /// Sweep all the BZZ from the node wallets into the Safe wallet.
+    SweepAll {
+        #[arg(long, default_value = "http://localhost:8545", help = "RPC to connect to")]
+        rpc: String,
+    },
+    /// Stake all the BZZ in the nodes' wallets
+    StakeAll {
+        #[arg(long, default_value = "http://localhost:8545")]
+        rpc: String,
+    },
+}
 
 /// A `clap` `value_parser` that parses a `NameOrAddress` from a string
 pub fn parse_name_or_address(s: &str) -> Result<H160> {
@@ -29,4 +231,239 @@ pub fn parse_bytes32(s: &str) -> Result<[u8; 32], String> {
     let s = strip_0x_prefix(s)?;
     hex::decode_to_slice(s, &mut bytes).map_err(|e| e.to_string())?;
     Ok(bytes)
+}
+
+/// Swarm tools CLI
+#[derive(Debug, Parser)]
+#[clap(name = "swarm-tools", version, author, about)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+
+pub async fn run(args: Cli) -> Result<()> {
+    match args.command {
+        Commands::Topology(topology) => match topology.command {
+            TopologyCommands::DumpBaseOverlays(radius) => {
+                let t = Topology::new(radius.radius);
+
+                println!("Base overlay addresses for radius {}:", radius.radius);
+                for i in 0..t.num_neighbourhoods() {
+                    println!("{}", hex::encode(t.get_base_overlay_address(i)));
+                }
+            }
+            TopologyCommands::NumNeighbourhoods(radius) => {
+                let t = Topology::new(radius.radius);
+
+                println!(
+                    "Number of neighbourhoods for radius {}: {}",
+                    radius.radius,
+                    t.num_neighbourhoods()
+                );
+            }
+            TopologyCommands::ActualAvgStorageRadius {
+                redistribution_address,
+                rpc,
+            } => {
+                let chain = chain::Chain::new(rpc).await?;
+
+                let (avg_depth, sample_size) = get_avg_depth(
+                    redistribution_address.unwrap_or(chain.get_address("REDISTRIBUTION").unwrap()),
+                    chain.client(),
+                )
+                .await?;
+
+                println!(
+                    "Average storage radius: {} (from {} samples)",
+                    avg_depth, sample_size
+                );
+            }
+        },
+        Commands::Overlay(overlay) => {
+            match overlay.command {
+                OverlayCommands::Calculate {
+                    address,
+                    network_id,
+                    nonce,
+                } => {
+                    println!(
+                        "Overlay address: 0x{}",
+                        hex::encode(address.overlay_address(network_id, nonce))
+                    );
+                }
+                OverlayCommands::Neighbourhood { radius, overlay } => {
+                    let t = Topology::new(radius);
+
+                    println!(
+                        "Neighbourhood for overlay {} with radius {} is {}",
+                        hex::encode(overlay),
+                        radius,
+                        t.get_neighbourhood(overlay)
+                    );
+                }
+                OverlayCommands::AutoMine {
+                    num_addresses,
+                    network_id,
+                    rpc,
+                } => {
+                    println!("Mining {} addresses...", num_addresses);
+
+                    // First need to get the average storage radius
+                    let chain = crate::chain::Chain::new(rpc).await?;
+
+                    let (avg_depth, sample_size) =
+                        get_avg_depth(chain.get_address("REDISTRIBUTION").unwrap(), chain.client())
+                            .await?;
+
+                    println!(
+                        "Average storage radius: {} (from {} samples)",
+                        avg_depth, sample_size
+                    );
+
+                    // Set the topology to the rounded avg_depth
+                    let t = Topology::new(avg_depth.round() as u32);
+
+                    // Now we need to find the optimal neighbourhoods for the given radius
+                    let mut game = Game::new(
+                        chain.get_address("STAKE_REGISTRY").unwrap(),
+                        chain.client(),
+                        &t,
+                    )
+                    .await?;
+
+                    let mut addresses = Vec::new();
+                    let mut total_new_stake = U256::from(0);
+
+                    loop {
+                        let (r, n) = game.find_optimum_neighbourhood();
+
+                        println!("Mining address into neighbourhood {} for radius {}", n, r);
+
+                        let eth_address = MinedAddress::new(r, n, network_id, None)?;
+                        let overlay_address = eth_address.overlay(network_id);
+
+                        // a sanity check to make sure the mined address is in the correct
+                        // neighbourhood
+                        let t2 = Topology::new(r);
+                        if t2.get_neighbourhood(overlay_address) != n {
+                            println!(
+                                "Neighbourhood for mined address: {}",
+                                t2.get_neighbourhood(overlay_address)
+                            );
+                        }
+                        assert!(t2.get_neighbourhood(overlay_address) == n);
+
+                        addresses.push(overlay_address);
+
+                        let new_player_stake = game.neighbourhood_avg_stake(t.get_neighbourhood(overlay_address));
+                        total_new_stake += new_player_stake;
+
+                        // add the player to the game, using the neighbourhood's average stake
+                        game.add_player(
+                            overlay_address,
+                            new_player_stake
+                        );
+
+                        // Check that the mined address is in a neighbourhood with a population
+                        // of 1
+                        let neighbourhood = game.view_by_radius(Some(r), Some(n));
+                        if neighbourhood.len() != 1 {
+                            println!("Neighbourhood: {:?}", neighbourhood);
+                        }
+                        assert!(neighbourhood.len() == 1);
+
+                        if addresses.len() == num_addresses as usize {
+                            break;
+                        }
+                    }
+
+                    game.stats();
+
+                    println!("Total new stake: {} BZZ", total_new_stake);
+                }
+                OverlayCommands::ManualMine {
+                    radius,
+                    neighbourhood,
+                    network_id,
+                    nonce,
+                } => {
+                    let t = Topology::new(radius);
+                    let mined_address =
+                        MinedAddress::new(radius, neighbourhood, network_id, nonce)?;
+
+                    println!(
+                        "Mined overlay address: 0x{}",
+                        hex::encode(mined_address.overlay(network_id))
+                    );
+
+                    println!(
+                        "Neighbourhood for overlay {} with radius {} is {}",
+                        hex::encode(mined_address.overlay(network_id)),
+                        radius,
+                        t.get_neighbourhood(mined_address.overlay(network_id))
+                    );
+
+                    println!(
+                        "Ethereum address: 0x{}",
+                        hex::encode(mined_address.address())
+                    );
+                    println!(
+                        "Private key: 0x{}",
+                        hex::encode(mined_address.private_key())
+                    );
+                    println!("Password: {}", mined_address.password());
+                }
+            }
+        }
+        Commands::Game {
+            stake_registry,
+            radius,
+            rpc,
+        } => {
+            let chain = crate::chain::Chain::new(rpc).await?;
+            let t = Topology::new(radius);
+
+            let game = Game::new(
+                stake_registry.unwrap_or(chain.get_address("STAKE_REGISTRY").unwrap()),
+                chain.client(),
+                &t,
+            )
+            .await?;
+
+            game.stats();
+        }
+        Commands::PostageStamp {
+            postage_stamp_contract_address,
+            start_block,
+            rpc,
+        } => {
+            let chain = crate::chain::Chain::new(rpc).await?;
+
+            let post_office = PostOffice::new(
+                postage_stamp_contract_address
+                    .unwrap_or(chain.get_address("POSTAGE_STAMP").unwrap()),
+                chain.client(),
+                start_block,
+            )
+            .await?;
+
+            println!("{}", post_office);
+
+            let num_chunks = post_office.num_chunks();
+            let total_size_gb = num_chunks * 4096 / 1024 / 1024 / 1024;
+            let round_reward = post_office.round_reward(152);
+
+            println!("Total chunks: {}", num_chunks);
+            println!("Total size: {} GB", total_size_gb);
+            println!(
+                "Round reward: {} BZZ",
+                ethers::utils::format_units(round_reward, 16)?
+            );
+        }
+        Commands::Wallet(args) => {
+            crate::wallet::process(args).await?
+        }
+    }
+
+    Ok(())
 }
---
+++ src/wallet.rs
@@ -0,0 +1,207 @@
+use std::{path::{Path, PathBuf}, collections::HashMap};
+
+use eyre::{anyhow, Result};
+use ethers::{prelude::k256::ecdsa::SigningKey, prelude::*, types::H160};
+use passwords::PasswordGenerator;
+
+use crate::{WalletArgs, WalletCommands};
+
+pub async fn process(args: WalletArgs) -> Result<()> {
+    match args.command {
+        WalletCommands::Generate => {
+            // Get the path to the wallet directory
+            let path = PathBuf::from(".");
+            let bees_dir = path.join("bees");
+
+            let mut store = WalletStore::load(bees_dir.clone()).unwrap();
+            if store.get("wallet".to_owned()).is_ok() {
+                return Err(anyhow!("Wallet already exists"));
+            }
+
+            // Wallet doesn't exist, so create a new one
+            let result = store.create_wallet(
+                &bees_dir,
+                None,
+                |_key| "wallet".to_string(), 
+                |_key| true
+            );
+
+            // If the wallet was created successfully, print the address
+            if let Ok((wallet, password)) = result {
+                println!("Wallet created: 0x{}", hex::encode(wallet.address()));
+                println!("Password: {}", password);
+            }
+
+            Ok(())
+        },
+        WalletCommands::InitSafe { rpc } => {
+            todo!()
+        },
+        WalletCommands::CalculateFundingRequirements { max_bzz, xdai, rpc } => {
+            todo!()
+        },
+        WalletCommands::SwapAndBridge { mainnet_rpc, gnosis_rpc, max_bzz, xdai } => {
+            todo!()
+        },
+        WalletCommands::DistributeFunds { max_bzz, xdai, rpc } => {
+            todo!()
+        },
+        WalletCommands::PermitApproveAll { rpc } => {
+            todo!()
+        },
+        WalletCommands::SweepAll { rpc } => {
+            todo!()
+        },
+        WalletCommands::StakeAll { rpc } => {
+            todo!()
+        },
+    }
+}
+
+pub struct WalletStore {
+    path: PathBuf,
+    wallets: HashMap<String, Wallet<SigningKey>>,
+    pg: PasswordGenerator,
+}
+
+impl WalletStore {
+    pub fn load(path: PathBuf) -> Result<Self> {
+        // The path should be a directory
+        if !path.is_dir() {
+            // create the directory if it doesn't exist
+            std::fs::create_dir_all(path.clone()).unwrap();
+        }
+
+        // Create a new wallet store
+        let mut store = Self {
+            path: path.clone(),
+            wallets: HashMap::new(),
+            pg: PasswordGenerator {
+                length: 32,
+                numbers: true,
+                lowercase_letters: true,
+                uppercase_letters: true,
+                symbols: false,
+                spaces: false,
+                exclude_similar_characters: true,
+                strict: true,
+            },
+        };
+
+        // Iterate over all the files in the directory. Any .json files are assumed to be keystore files
+        for entry in path.read_dir()? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.extension().unwrap_or_default() == "json" {
+                store.load_wallet(&path)?;
+            }
+        }
+
+        Ok(store)
+    }
+
+    pub fn load_wallet(&mut self, path: &Path) -> Result<()> {
+        // We assume that the path is a file to the encrypted keystore
+        if !path.is_file() {
+            return Err(anyhow!("Invalid keystore path"));
+        }
+
+        // The password to load the keystore is in the file with the same name as the keystore but with a .password extension
+        let password_path = path.with_extension("password");
+        if !password_path.is_file() {
+            return Err(anyhow!("Password file not found"));
+        }
+
+        // Load the password from the file
+        std::fs::read_to_string(password_path)
+            .map_err(|e| anyhow!("Failed to read password file: {}", e))
+            .and_then(|password| {
+                // Load the wallet from the keystore
+                let wallet = LocalWallet::decrypt_keystore(path, password)?;
+                // Get the wallet name from the file name (strip the extension)
+                let name = path.file_stem().unwrap().to_str().unwrap().to_string();
+
+                println!("Loaded wallet: {}", name);
+
+                // Add the wallet to the store
+                self.wallets.insert(name, wallet);
+                Ok(())
+            })
+
+    }
+
+    pub fn insert_wallet(&mut self, name: String, wallet: Wallet<SigningKey>) -> Result<()> {
+        if self.wallets.contains_key(&name) {
+            return Err(anyhow!("Wallet already exists"));
+        }
+
+        self.wallets.insert(name, wallet);
+        Ok(())
+    }
+
+    pub fn get(&self, name: String) -> Result<Wallet<SigningKey>> {
+        match self.wallets.get(&name) {
+            Some(wallet) => Ok(wallet.clone()),
+            None => Err(anyhow!("Wallet not found")),
+        }
+    }
+
+    pub fn get_address(&self, name: String) -> Result<H160> {
+        self.get(name).map(|wallet| wallet.address())
+    }
+
+    pub fn get_name(&self, address: H160) -> Result<String> {
+        for (name, wallet) in self.wallets.iter() {
+            if wallet.address() == address {
+                return Ok(name.clone());
+            }
+        }
+
+        Err(anyhow!("Wallet not found"))
+    }
+
+    pub fn create_wallet<F, N>(
+        &mut self,
+        path: &Path,
+        password: Option<String>,
+        name: N,
+        verify: F,
+    ) -> Result<(Wallet<SigningKey>, String)> 
+        where F: FnOnce(Wallet<SigningKey>) -> bool, 
+            N: FnOnce(Wallet<SigningKey>) -> String
+    {
+        // If the password is not provided, generate a random one
+        let password = match password {
+            Some(password) => password,
+            None => self.pg.generate_one().unwrap(),
+        };
+
+        // create a new keystore with the password
+        let (wallet, uuid) =
+            LocalWallet::new_keystore(path, &mut rand::thread_rng(), password.clone(), None)?;
+
+        // Verify the wallet
+        if !verify(wallet.clone()) {
+            return Err(anyhow!("Wallet verification failed"));
+        }
+
+        // Get the name of the wallet
+        let name = name(wallet.clone());
+
+        let keystore_path = path.join(format!("{}", uuid));
+        let new_keystore_path = self.path.join(format!("{}.json", name));
+        println!("{} -> {}", keystore_path.display(), new_keystore_path.display());
+        std::fs::copy(keystore_path, new_keystore_path)?;
+
+        // Save the password to the wallet store path
+        let password_path = self.path.join(format!("{}.password", name));
+        std::fs::write(password_path, password.clone())?;
+
+        // Add the wallet to the wallet store
+        self.insert_wallet(name.clone(), wallet.clone())?;
+
+        // return the path to the keystore and the password
+        Ok((wallet, password))
+    }
+
+}
