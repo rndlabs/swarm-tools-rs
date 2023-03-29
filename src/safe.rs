cc7576e3c26b049f7e51a54990fe688f5c270557
mfw78 <mfw78@protonmail.com>
2023-03-29 10:29:42 +0000

    fix: lint

--- src/safe.rs
+++ src/safe.rs
@@ -1,9 +1,9 @@
 use std::{str::FromStr, sync::Arc};
 
-use ethers::{prelude::*, prelude::k256::ecdsa::SigningKey};
 use crate::chain::Chain;
 use crate::contracts::gnosis_proxy_factory::ProxyCreationFilter;
 use crate::contracts::{gnosis_proxy_factory::GnosisProxyFactory, gnosis_safe_l2::GnosisSafeL2};
+use ethers::{prelude::k256::ecdsa::SigningKey, prelude::*};
 
 // Declare constants
 const MULTI_SEND_ADDRESS: &str = "0xA238CBeb142c10Ef7Ad8442C6D1f9E89e07e7761";
@@ -11,7 +11,8 @@ const PROXY_FACTORY_ADDRESS: &str = "0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2"
 const GNOSIS_SAFE_L2_ADDRESS: &str = "0x3E5c63644E683549055b9Be8653de26E0B4CD36E";
 const COMPATIBILITY_FALLBACK_HANDLER: &str = "0x017062a1dE2FE6b99BE3d9d37841FeD19F573804";
 
-const FALLBACK_HANDLER_SLOT: &str = "0x6c9a6c4a39284e37ed1cf53d337577d14212a4870fb976a4366c693b939918d5";
+const FALLBACK_HANDLER_SLOT: &str =
+    "0x6c9a6c4a39284e37ed1cf53d337577d14212a4870fb976a4366c693b939918d5";
 
 // Declare the Safe struct
 #[derive(Debug, Clone, Default, Eq, PartialEq)]
@@ -40,25 +41,34 @@ impl Safe {
     ) -> Self {
         let client = chain.client();
 
-        let singleton = GnosisSafeL2::new(H160::from_str(GNOSIS_SAFE_L2_ADDRESS).unwrap(), client.clone());
-        let signer = SignerMiddleware::new(client.clone(), wallet.clone().with_chain_id(chain.chain_id()));
-        let contract = GnosisProxyFactory::new(H160::from_str(PROXY_FACTORY_ADDRESS).unwrap(), signer.into());
-
-        let cd = singleton.setup(
-            owners,
-            threshold,
-            Address::zero(),
-            vec![].into(),
-            fallback_handler.unwrap_or(H160::from_str(COMPATIBILITY_FALLBACK_HANDLER).unwrap()),
-            Address::zero(),
-            U256::from(0),
-            Address::zero()
-        ).calldata().unwrap();
-
-        let call = contract.create_proxy(
+        let singleton = GnosisSafeL2::new(
             H160::from_str(GNOSIS_SAFE_L2_ADDRESS).unwrap(),
-            cd
+            client.clone(),
         );
+        let signer = SignerMiddleware::new(
+            client.clone(),
+            wallet.clone().with_chain_id(chain.chain_id()),
+        );
+        let contract = GnosisProxyFactory::new(
+            H160::from_str(PROXY_FACTORY_ADDRESS).unwrap(),
+            signer.into(),
+        );
+
+        let cd = singleton
+            .setup(
+                owners,
+                threshold,
+                Address::zero(),
+                vec![].into(),
+                fallback_handler.unwrap_or(H160::from_str(COMPATIBILITY_FALLBACK_HANDLER).unwrap()),
+                Address::zero(),
+                U256::from(0),
+                Address::zero(),
+            )
+            .calldata()
+            .unwrap();
+
+        let call = contract.create_proxy(H160::from_str(GNOSIS_SAFE_L2_ADDRESS).unwrap(), cd);
 
         // Estimate the gas limit
         let gas_limit = client.estimate_gas(&call.tx, None).await.unwrap();
@@ -69,7 +79,10 @@ impl Safe {
         // Confirm with the user
         println!("Gas limit: {}", gas_limit);
         println!("Gas price: {}", gas_price);
-        println!("Total gas cost: {} xDAI", ethers::utils::format_units(gas_limit * gas_price, "ether").unwrap());
+        println!(
+            "Total gas cost: {} xDAI",
+            ethers::utils::format_units(gas_limit * gas_price, "ether").unwrap()
+        );
 
         // Make sure the user has enough funds. If not, print a helpful message with how much they need
         let balance = client.get_balance(wallet.address(), None).await.unwrap();
@@ -98,7 +111,9 @@ impl Safe {
         if let Err(e) = tx {
             // if the error message contains "insufficient funds", print a more helpful message
             if e.to_string().contains("insufficient funds") {
-                println!("Transaction failed: insufficient funds. Please check your wallet balance.");
+                println!(
+                    "Transaction failed: insufficient funds. Please check your wallet balance."
+                );
             } else {
                 println!("Transaction failed: {:?}", e);
             }
@@ -109,7 +124,10 @@ impl Safe {
         let tx = tx.unwrap();
 
         // Print URL for Gnosisscan
-        println!("Submitting the transaction to Gnosis Chain... https://gnosisscan.io/tx/0x{}", hex::encode(tx.tx_hash()));
+        println!(
+            "Submitting the transaction to Gnosis Chain... https://gnosisscan.io/tx/0x{}",
+            hex::encode(tx.tx_hash())
+        );
         println!("Waiting for the transaction to be mined...");
 
         // Wait for the transaction to be mined
@@ -121,14 +139,21 @@ impl Safe {
         // contract.event::<ProxyCreationFilter>().parse_log(log)
 
         // iterate over the logs to find the ProxyCreated event and get the address of the Safe
-        let safe_address = receipt.logs.iter().find_map(|log| {
-            if log.address == H160::from_str(PROXY_FACTORY_ADDRESS).unwrap() {
-                let event = contract.event::<ProxyCreationFilter>().parse_log(log.clone()).unwrap();
-                Some(event.proxy)
-            } else {
-                None
-            }
-        }).unwrap();
+        let safe_address = receipt
+            .logs
+            .iter()
+            .find_map(|log| {
+                if log.address == H160::from_str(PROXY_FACTORY_ADDRESS).unwrap() {
+                    let event = contract
+                        .event::<ProxyCreationFilter>()
+                        .parse_log(log.clone())
+                        .unwrap();
+                    Some(event.proxy)
+                } else {
+                    None
+                }
+            })
+            .unwrap();
 
         println!("Safe deployed at: {}", safe_address);
 
@@ -136,10 +161,7 @@ impl Safe {
     }
 
     /// Loads a Safe instance from an address already deployed to the L2 network
-    pub async fn load(
-        address: H160,
-        client: Arc<Provider<Http>>,
-    ) -> Self {
+    pub async fn load(address: H160, client: Arc<Provider<Http>>) -> Self {
         let safe = GnosisSafeL2::new(address, client.clone());
 
         // use multicall to get the safe info
@@ -148,7 +170,13 @@ impl Safe {
         multicall.add_call(safe.get_threshold(), false);
         multicall.add_call(safe.get_owners(), false);
         multicall.add_call(safe.get_storage_at(U256::from(0), U256::from(1)), false);
-        multicall.add_call(safe.get_storage_at(U256::from_str(FALLBACK_HANDLER_SLOT).unwrap(), U256::from(1)), false);
+        multicall.add_call(
+            safe.get_storage_at(
+                U256::from_str(FALLBACK_HANDLER_SLOT).unwrap(),
+                U256::from(1),
+            ),
+            false,
+        );
         multicall.add_call(safe.version(), false);
         multicall.add_call(safe.domain_separator(), false);
         multicall.add_get_chain_id();
@@ -164,14 +192,14 @@ impl Safe {
             (bool, U256),
         ) = multicall.call().await.unwrap();
 
-        let nonce = results.0.1;
-        let threshold = results.1.1;
-        let owners = results.2.1;
-        let master_copy = H160::from_slice(&results.3.1.0[12..]);
-        let fallback_handler = H160::from_slice(&results.4.1.0[12..]);
-        let version = results.5.1;
-        let domain_separator = results.6.1;
-        let chain_id = results.7.1;
+        let nonce = results.0 .1;
+        let threshold = results.1 .1;
+        let owners = results.2 .1;
+        let master_copy = H160::from_slice(&results.3 .1 .0[12..]);
+        let fallback_handler = H160::from_slice(&results.4 .1 .0[12..]);
+        let version = results.5 .1;
+        let domain_separator = results.6 .1;
+        let chain_id = results.7 .1;
 
         Self {
             address,
@@ -185,5 +213,4 @@ impl Safe {
             chain_id,
         }
     }
-
 }
